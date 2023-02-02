5118d8bc7dd39f1be4deb5fee57e641b21383905
tim90721 <tim90721@gmail.com>
2023-02-03 01:04:43 +0800

    04-trap: irq: add header
    add irq header with IRQ enumeration and basic irq handler function
    prototype.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

---
+++ src/04-trap/inc/irq.h
@@ -0,0 +1,14 @@
+#ifndef _IRQ_H_
+#define _IRQ_H_
+
+typedef void (*irq_handler_t)(void);
+
+enum irq_id {
+	IRQ_VIRTIO = 1,
+	IRQ_VIRTIO_CNT = 8,
+	IRQ_UART0 = 10,
+	IRQ_RTC = 11,
+	IRQ_PCIE = 32,
+	__IRQ_MAX_ID,
+};
+#endif /* _IRQ_H_ */
