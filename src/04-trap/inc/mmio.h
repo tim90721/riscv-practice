1844188e9897efc845aa0fb7653bad8a23e2e4db
tim90721 <tim90721@gmail.com>
2023-02-03 01:03:37 +0800

    04-trap: mmio.h: add 32-bit set/clr API
    add 32-bit set/clr API.
    
    Signed-off-by: tim90721 <tim90721@gmail.com>

--- src/04-trap/inc/mmio.h
+++ src/04-trap/inc/mmio.h
@@ -23,6 +23,22 @@ static inline u16 mmio_read_16(uintptr addr)
 	return *(volatile u16 *)addr;
 }
 
+static inline void mmio_setbits_32(uintptr addr, u32 set)
+{
+	*(volatile u32 *)addr |= set;
+}
+
+static inline void mmio_clrbits_32(uintptr addr, u32 clr)
+{
+	*(volatile u32 *)addr &= (~clr);
+}
+
+static inline void mmio_clrsetbits_32(uintptr addr, u32 clr, u32 set)
+{
+	*(volatile u32 *)addr &= (~clr);
+	*(volatile u32 *)addr |= set;
+}
+
 static inline void mmio_write_32(uintptr addr, u32 val)
 {
 	*(volatile u32 *)addr = val;
